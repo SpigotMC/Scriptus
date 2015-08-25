b24abce1450a5b892ff0a5b7058837a3645bb9ff
sha-2 <me@sha-2.net>
2015-08-26 01:07:41 +0200

    Remove the import formatting

--- src/main/java/net/md_5/scriptus/DescribeMojo.java
+++ src/main/java/net/md_5/scriptus/DescribeMojo.java
@@ -5,9 +5,7 @@ import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
-
 import java.io.File;
-
 import org.apache.maven.project.MavenProject;
 import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.errors.RepositoryNotFoundException;
